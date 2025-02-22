14557bc7d1aded42cfb9261e6e67b43b31439618
Tomáš Juřička <tomas.juricka@outlook.cz>
2025-02-22 12:12:40 +0100

    Fix packages

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,7 +1,7 @@
 import sitemap from "@astrojs/sitemap";
-import { defineConfig } from "astro/config";
-
+import tailwindcss from "@tailwindcss/vite";
 import robotsTxt from "astro-robots-txt";
+import { defineConfig } from "astro/config";
 
 // https://astro.build/config
 export default defineConfig({
